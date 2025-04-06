b0bc912867b1766a3d6d429591e8757c19143d84
zebinho20-belenus <54846054+zebinho20-belenus@users.noreply.github.com>
2025-04-06 16:21:07 +0200

    bug paypal cancel add own sessionManager

--- module/User/src/User/Manager/UserSessionManagerFactory.php
+++ module/User/src/User/Manager/UserSessionManagerFactory.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 namespace User\Manager;
 
 use Zend\ServiceManager\FactoryInterface;
@@ -16,49 +16,6 @@ class UserSessionManagerFactory implements FactoryInterface
             $sm->get('Zend\Session\SessionManager'));
     }
 
-}*/
-
-
-namespace User\Manager;
-
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\Session\Config\SessionConfig;
-use Zend\Session\SessionManager;
-use Zend\Session\Validator\RemoteAddr;
-use Zend\Session\Validator\HttpUserAgent;
-
-class UserSessionManagerFactory implements FactoryInterface
-{
-    public function createService(ServiceLocatorInterface $sm)
-    {
-        $configManager = $sm->get('Base\Manager\ConfigManager');
-        $userManager = $sm->get('User\Manager\UserManager');
-
-        $sessionSettings = $configManager->get('session_config');
-
-        $sessionConfig = new SessionConfig();
-
-        // Fehler vermeiden: Session darf NICHT aktiv sein beim save_handler setzen!
-        if (session_status() === PHP_SESSION_NONE) {
-            $sessionConfig->setOptions($sessionSettings);
-        }
-
-        $sessionManager = new SessionManager($sessionConfig);
-
-        // Validatoren aktivieren (optional aber empfohlen)
-        $sessionManager->getValidatorChain()->attach('session.validate', [new RemoteAddr(), 'isValid']);
-        $sessionManager->getValidatorChain()->attach('session.validate', [new HttpUserAgent(), 'isValid']);
-
-        // Session nur starten, wenn noch nicht aktiv (wichtig für PayPal Redirects)
-        if (session_status() === PHP_SESSION_NONE) {
-            $sessionManager->start();
-        }
-
-        return new UserSessionManager(
-            $configManager,
-            $userManager,
-            $sessionManager
-        );
-    }
 }
+
+
