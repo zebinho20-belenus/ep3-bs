ed3bf804d14522afc37617071a4f47cfeafe0f52
zebinho20-belenus <54846054+zebinho20-belenus@users.noreply.github.com>
2025-04-06 15:00:24 +0200

    bug paypal cancel add own sessionManager

---
+++ module/User/src/User/Manager/CustomSessionManagerFactory.php
@@ -0,0 +1,34 @@
+<?php
+namespace User\Manager;
+
+use Zend\Session\Config\SessionConfig;
+use Zend\Session\SessionManager;
+use Zend\Session\Container;
+
+class CustomSessionManagerFactory
+{
+    public function __invoke($container)
+    {
+        $config = new SessionConfig();
+
+        if (session_status() === PHP_SESSION_NONE) {
+            $config->setOptions([
+                'save_handler' => 'files',
+                'save_path' => '/data/session/',
+                'name' => 'platzbuchung',
+                'cookie_httponly' => true,
+            ]);
+        }
+
+        $manager = new SessionManager($config);
+        Container::setDefaultManager($manager);
+
+        if (session_status() === PHP_SESSION_NONE) {
+            $manager->start();
+        }
+
+        return $manager;
+    }
+}
+
+
